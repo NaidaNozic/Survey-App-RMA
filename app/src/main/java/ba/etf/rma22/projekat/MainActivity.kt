6334f50f25ccff466146d69ca4f3f720f7ddb7fc
NaidaNozic <92310390+NaidaNozic@users.noreply.github.com>
2022-03-31 13:23:44 +0200

    napravljen activity UpisIstrazivanje i povezala sa dugmetom upisDugme

--- app/src/main/java/ba/etf/rma22/projekat/MainActivity.kt
+++ app/src/main/java/ba/etf/rma22/projekat/MainActivity.kt
@@ -1,6 +1,7 @@
 package ba.etf.rma22.projekat
 
 import AnketaListAdapter
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
@@ -8,12 +9,14 @@ import android.widget.Spinner
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import ba.etf.rma22.projekat.viewmodel.AnketaListViewModel
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
     private lateinit var ankete: RecyclerView
     private lateinit var anketeAdapter: AnketaListAdapter
     private var anketeListViewModel = AnketaListViewModel()
     private lateinit var spinner: Spinner
+    private lateinit var button: FloatingActionButton
 
     var elementi_spinnera = arrayOf("Sve moje ankete", "Sve ankete",
         "Urađene ankete", "Buduće ankete", "Prošle (neurađene) ankete")
@@ -21,6 +24,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //button
+        button=findViewById(R.id.upisDugme)
+        button.setOnClickListener{
+            val intent = Intent(this, UpisIstrazivanje::class.java)
+            startActivity(intent)
+        }
 
         //spinner
         spinner=findViewById(R.id.filterAnketa)
